a7997a0c7b487413914b97e14e02b4b9e209e841
Aaron Chewlun <ache0084@student.monash.edu>
2023-09-08 15:40:20 +1000

    vega lite :3

---
+++ week7/vega_lite_vis.js
@@ -0,0 +1,5 @@
+let vg_1 = "daily_cost_bar_chart.vg.json";
+
+// Access the vega view instance
+// (https://vega.github.io/vega/docs/api/view/) as result.view
+vegaEmbed("#bar_chart", vg_1).then(function (result) { }).catch(console.error);
