fd4087ed7087a0200447b294bce5bfe9057bf6fd
Aaron Chewlun <ache0084@student.monash.edu>
2023-09-08 16:03:52 +1000

    copied bar chart :3

---
+++ week7/task2/vis.js
@@ -0,0 +1,5 @@
+let vg_1 = "euro_cities_barchart.vg.json";
+
+// Access the vega view instance
+// (https://vega.github.io/vega/docs/api/view/) as result.view
+vegaEmbed("#bar_chart", vg_1).then(function (result) { }).catch(console.error);
