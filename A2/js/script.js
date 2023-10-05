a6085c9f42ed1536c47bcc3aba5104fb60cee5a9
Aaron Chewlun <ache0084@student.monash.edu>
2023-10-05 15:05:43 +1100

    rearranged files/folders

--- A2/js/script.js
+++ A2/js/script.js
@@ -1,29 +1,29 @@
-var symbolMap = "js/symbol_map.vg.json";
+var symbolMap = "js/charts/symbol_map.vg.json";
 vegaEmbed('#symbol_map', symbolMap).then(function (result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var deathsBarchart = "js/deaths_barchart.vg.json";
+var deathsBarchart = "js/charts/deaths_barchart.vg.json";
 vegaEmbed('#deaths_barchart', deathsBarchart).then(function (result) {
 
 }).catch(console.error);
 
-var costBarchart = "js/cost_barchart.vg.json";
+var costBarchart = "js/charts/cost_barchart.vg.json";
 vegaEmbed('#cost_barchart', costBarchart).then(function (result) {
 
 }).catch(console.error);
 
-var affectedScatterplot = "js/affected_scatterplot.vg.json";
+var affectedScatterplot = "js/charts/affected_scatterplot.vg.json";
 vegaEmbed('#affected_scatterplot', affectedScatterplot).then(function (result) {
 
 }).catch(console.error);
 
-var regionPiechart = "js/region_piechart.vg.json";
+var regionPiechart = "js/charts/region_piechart.vg.json";
 vegaEmbed('#region_piechart', regionPiechart).then(function (result) {
 
 }).catch(console.error);
 
-var dtypePieChart = "js/dtype_piechart.vg.json";
+var dtypePieChart = "js/charts/dtype_piechart.vg.json";
 vegaEmbed('#dtype_piechart', dtypePieChart).then(function (result) {
 
 }).catch(console.error);
